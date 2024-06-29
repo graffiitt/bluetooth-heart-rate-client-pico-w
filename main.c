8e0b20cf2f06babbece13e326955624c47bedfe2
vlad mac <vladikkopylov31@gmail.com>
2024-06-29 22:35:40 +0300

    [ADD] init

---
+++ main.c
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2023 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "btstack.h"
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+#include <inttypes.h>
+
+ 
+int main()
+{
+    stdio_init_all();
+    if (cyw43_arch_init())
+    {
+        printf("failed to initialise cyw43_arch\n");
+        return -1;
+    }
+    hci_power_control(HCI_POWER_ON);
+
+ 
+    while (1)
+    {
+        /* code */
+    }
+
+    return 0;
+}
