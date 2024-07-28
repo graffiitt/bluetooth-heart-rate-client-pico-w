af24fcc05ba8c5b9f153b09ef52044d3b9c00dda
vlad mac <vladikkopylov31@gmail.com>
2024-07-29 00:19:11 +0300

    add search devices

--- main.c
+++ main.c
@@ -1,28 +1,22 @@
-/**
- * Copyright (c) 2023 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
+#include "bluetooth/ble_client.h"
 
-#include <stdio.h>
-#include "btstack.h"
-#include "pico/cyw43_arch.h"
-#include "pico/stdlib.h"
-#include <inttypes.h>
-
-extern int btstack_mains();
+extern cvector(struct BLE_Item) bleItems;
 
 int main()
 {
+    timer_hw->dbgpause = 0x2 | 0x4;
     stdio_init_all();
-    if (cyw43_arch_init())
-    {
-        printf("failed to initialise cyw43_arch\n");
-        return -1;
-    }
-    hci_power_control(HCI_POWER_ON);
-    btstack_mains();
 
+    ble_init();
+
+    ble_scan_start();
+    sleep_ms(10000);
+    ble_scan_stop();
+
+    for (int i = 0; i < cvector_size(bleItems); i++)
+    {
+        printf("\n%d id  %s name %s \n", i, cvector_at(bleItems, i)->name, bd_addr_to_str(cvector_at(bleItems, i)->address));
+    }
     while (1)
     {
         /* code */
